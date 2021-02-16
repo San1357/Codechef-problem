c33f30193e1d1d08467900c5373b5e47a68f6ca6
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-16 17:19:58 +0530

    Create  Helping_dustin.py

---
+++ HelpingDustin/Helping_dustin.py
@@ -0,0 +1,27 @@
+#Problem Code:ICL1904
+
+
+
+
+
+#CODE:
+
+
+t = int(input())
+
+for _ in range(t):
+
+    n,s = input().split()
+
+    N = int(n) 
+    r = N - len(s)
+
+    count = 0
+
+    if N>len(s):
+
+        count = pow(26, r-1,(10**9+7))
+        count*= (26+25*len(s))
+
+        count = count%(10**9 + 7)
+    print(count)
